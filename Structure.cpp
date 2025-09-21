06296cdb673f5e23ea8d3eb55b8e86237f9c3825
shafayatGit <shafayathossain.ielts@gmail.com>
2025-09-21 12:55:42 +0600

    Structure Basic cpp

--- Structure.cpp
+++ Structure.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 struct Rectangle
 {
     int length, breadth; // 2 bytes + 2 bytes = 4 Bytes
@@ -5,8 +7,9 @@ struct Rectangle
 
 int main()
 {
-    struct Rectangle r; // Declaration
-    // struct Rectangle r = {10,5} ----->>> Declaration + Initialization
-    r.length = 10; //can access using dot(.)
+    //struct Rectangle r; //Only Declaration
+    struct Rectangle r = {10,5};// ----->>> Declaration + Initialization
+    r.length = 15; //can access using dot(.)
     r.breadth = 5; 
+    cout<<"Area is:"<<r.length * r.breadth;
 }
