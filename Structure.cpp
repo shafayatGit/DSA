df39c97a93f6eb826e6a96871d2223e9bf9437b8
shafayatGit <shafayathossain.ielts@gmail.com>
2025-09-21 15:36:23 +0600

    structure basic with memory

--- Structure.cpp
+++ Structure.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 struct Rectangle
 {
-    int length, breadth; // 2 bytes + 2 bytes = 4 Bytes
+    int length, breadth; // 4 bytes + 4 bytes = 8 Bytes
+    //char x; character takes 1 byte but full stucture will take 4+4+4 = 12 bytes
 };
 
 int main()
@@ -11,5 +12,8 @@ int main()
     struct Rectangle r = {10,5};// ----->>> Declaration + Initialization
     r.length = 15; //can access using dot(.)
     r.breadth = 5; 
-    cout<<"Area is:"<<r.length * r.breadth;
+    cout<<"Area is:"<<r.length * r.breadth<<endl;
+    cout<<sizeof(r.length)<<endl; //----> That will take 8 bytes bcz of the structure is 8 bytes
+    cout<<sizeof(r.breadth)<<endl; //----> That will take 8 bytes bcz of the structure is 8 bytes
+    //cout<<sizeof(r); //----> That will take 16 bytes bcz of the structure is 8+8 bytes
 }
