af39fae00145aed26470b3a6f840797b02ee9353
shafayatGit <shafayathossain.ielts@gmail.com>
2025-09-25 11:04:30 +0600

    class and constructor

---
+++ ClassAndConstructorC++.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+class Rectangle // class is same as JAVA
+{
+private:
+    int length, breadth;
+
+public:
+    Rectangle(int l, int b) // making constructor like JAVA
+    {
+        length = l;
+        breadth = b;
+    }
+
+    void changeLength(int l)
+    {
+        length = l;
+    }
+
+    int area()
+    {
+        return length * breadth;
+    }
+
+    int getLength()
+    { // getter
+        return length;
+    }
+    int getBreadth()
+    { // getter
+        return breadth;
+    }
+};
+
+int main()
+{
+    Rectangle r(10, 5); // This is now an object like JAVA
+
+    cout << "Initial Length:" << r.getLength() << " Initial Breadth:" << r.getBreadth() << endl;
+
+    cout << "Area:" << r.area() << endl;
+
+    r.changeLength(20); // changing the length of the structure.
+    cout << "Changed Length:" << r.getLength() << endl;
+}
